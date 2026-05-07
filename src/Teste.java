52737df59396fb8a28ce338d37eaff8c50d2873f
kauancampina <kauan.campina@gmail.com>
2026-05-06 21:39:13 -0300

    Estou realizando um teste de Commit e push para o Felipe Ver.

--- src/Teste.java
+++
@@ -1,5 +0,0 @@
-public class Teste {
-    static void main(String[] args) {
-
-    }
-}
---
+++ src/Teste02.java
@@ -0,0 +1,7 @@
+public class Teste02 {
+    static void main(String[] args) {
+
+        System.out.println("Testando");
+
+    }
+}
